d2e3a5a080ea177b1065618111fb6757d41f53fc
Igor Brown <ibrownramos@gmail.com>
2022-04-05 15:25:44 -0300

    feat: refatored methods

--- app/gilded-rose.ts
+++ app/gilded-rose.ts
@@ -1,4 +1,10 @@
 import { Item } from './item';
+import { 
+  updateQualityForAgedBrie, 
+  updateQualityForConjured,
+  updateQualityDefaultItem,
+  updateQualityForSulfuras,
+} from './update-quality';
 
 export class GildedRose {
   items: Item[];
@@ -11,7 +17,23 @@ export class GildedRose {
     
     this.items.forEach((item: Item) => {
       
-      
+      switch (item.name) {
+        case 'Aged Brie': {
+          item = updateQualityForAgedBrie(item);
+          break;
+        }
+        case 'Conjured': {
+          item = updateQualityForConjured(item);
+          break;
+        }
+        case 'Sulfuras': {
+          item = updateQualityForSulfuras(item);
+          break;
+        }
+        default: {
+            item = updateQualityDefaultItem(item);
+        }
+      }
 
     });
 
@@ -25,8 +47,9 @@ const item1 = new Item('Aged Brie', 0, 0);
 const item2 = new Item('Backstage passes', 0, 20); 
 const item3 = new Item('Conjured', 4, 20); 
 
-const gildedRose = new GildedRose(
-  [item1, item2, item3]);
+const gildedRose = new GildedRose([
+  item1, item2, item3
+]);
 
 const items = gildedRose.updateQuality();
 
--- app/update-quality.ts
+++ app/update-quality.ts
@@ -10,43 +10,57 @@ const addQuality = (quality: number) => isMax(quality) ? quality + 1 : quality;
 const removeQuality = (quality: number) => isMin(quality) ? quality - 1 :  quality; 
 
 export const updateQualityForAgedBrie = (item: Item) :Item =>  {
-  // Implement method
+  item.quality = addQuality(item.quality)
+  item.quality = item.sellIn < 0 ? addQuality(item.quality) : item.quality;
+  item.sellIn -= 1;
 
   return item;
 }
 
-const increaseQualityForConcert = (item: Item): number => {
-  // Implement method
+const addQualityForConcert = (item: Item): number => {
+  let quality = addQuality(item.quality);
+  quality = item.sellIn < 11 ? addQuality(quality) : quality;
+  quality = item.sellIn < 6 ? addQuality(quality) : quality;
 
-  return 0; //quality
+  return quality;
 }
 
 export const updateQualityForConcert = (item: Item) :Item => {
-  // Implement method
+  item.quality = item.sellIn === 0 ? 0 : addQualityForConcert(item);    
+  item.sellIn -= 1;
 
   return item;
 }
 
 export const updateQualityForSulfuras = (item: Item) :Item => {
-  // Implement method
+  item.quality = 80;
 
   return item;
 }
 
 export const updateQualityForConjured = (item: Item) :Item => {
-  // Implement method
+  if (item.sellIn === 5) {
+    item.quality -= 3;
+  } else {
+    item = updateQualityItem(item)
+    item = updateQualityItem(item);
+  }
+
+  item.sellIn -= 1;
 
   return item;
 }
 
 const updateQualityItem = (item: Item): Item => {
-  // Implement method
+  item.quality = removeQuality(item.quality);
+  item.quality = item.sellIn <= 0 ? removeQuality(item.quality) : item.quality;
   
   return item;
 }
 
-export const updateQualityForNormalItem = (item: Item) :Item => { 
-  // Implement method
+export const updateQualityDefaultItem = (item: Item) :Item => { 
+  item = updateQualityItem(item);
+  item.sellIn -= 1;
 
   return item;
 }
